4cf4d957f4b4b40bcf84efa6610feed0f3cc4f8a
julianperezm <julian.perezmunoz96@gmail.com>
2020-11-18 13:45:50 +0100

    changing the scene..

--- ThirdExercise/Components/Components.js
+++ ThirdExercise/Components/Components.js
@@ -56,7 +56,7 @@ AFRAME.registerComponent('changeattribute',{
 		let el = this.el;
 
 		el.addEventListener('grab-start', function () {
-			let entityToChange = el.parentNode.parentNode
+			let entityToChange = el.parentNode
 			entityToChange.setAttribute('material', el.getAttribute('material'))
 		});
 	}
@@ -71,7 +71,20 @@ AFRAME.registerComponent('editentity', {
 		let data = this.data;
 
 		this.el.addEventListener('grab-start', function(){
-			let tableForEdit = document.createElement('a-entity')
+			let colors = data.colors
+
+			for (let color of colors){
+				let colorButton = document.createElement('a-entity');
+				colorButton.setAttribute('geometry', {primitive:'box', width:0.1, height:0.1, depth:0.025});
+				colorButton.setAttribute('class', 'button');
+				colorButton.setAttribute('position', ((colors.indexOf(color)*0.2)-0.3)+" 0.7 0.01");
+				colorButton.setAttribute('material','color :' + color);
+				colorButton.setAttribute('clickable', {});
+				colorButton.setAttribute('changeattribute', {})
+				el.appendChild(colorButton)
+			}
+
+			/*let tableForEdit = document.createElement('a-entity')
 			tableForEdit.setAttribute('geometry', {primitive:'plane', width:1, height:0.5});
 			tableForEdit.setAttribute('material','color:white');
 			tableForEdit.setAttribute('stretchable', {});
@@ -90,7 +103,7 @@ AFRAME.registerComponent('editentity', {
 				tableForEdit.appendChild(colorButton)
 			}
 
-			el.appendChild(tableForEdit)
+			el.appendChild(tableForEdit)*/
 		});
 
 	}
@@ -131,13 +144,13 @@ AFRAME.registerComponent('podium',{
 	init: function(){
 		console.log("dentro del componente podium");
 		let podium = document.createElement('a-sphere');
-		podium.setAttribute('position',{x:0,y:0,z:-7});
-		podium.setAttribute('radius', 0.08)
-		podium.setAttribute('color', 'red')
-		podium.setAttribute('id', 'podium')
+		podium.setAttribute('position',{x:-0.4,y:2,z:0});
+		podium.setAttribute('radius', 0.01);
+		podium.setAttribute('color', 'red');
+		podium.setAttribute('id', 'podium');
 		console.log(podium);
 		//document.getElementById('podium').appendChild(podium)
-		let j = document.getElementsByClassName('podium')
+		let j = document.getElementsByClassName('podium');
 		j[0].appendChild(podium)
 	},
 });
@@ -178,20 +191,20 @@ AFRAME.registerComponent('menu',{
 	init: function(){
 		console.log("dentro del componente menu");
 		let baseMenu = document.createElement('a-cylinder');
-		baseMenu.setAttribute('position',{x:-0.5,y:1,z:-1});
+		baseMenu.setAttribute('position',{x:-2,y:1.2,z:0});
 		baseMenu.setAttribute('rotation', {x:0, y:0, z:0});
-		baseMenu.setAttribute('height', '0.02');
-		baseMenu.setAttribute('radius', '0.3');
+		baseMenu.setAttribute('height', '0.04');
+		baseMenu.setAttribute('radius', '0.7');
 		baseMenu.setAttribute('color', 'green');
 		baseMenu.setAttribute('animation', 'property:rotation;to:0 360 0;loop:true;dur:10000')
 		document.getElementById('menu').appendChild(baseMenu)
 		console.log(baseMenu);
 
 		let figure1 = document.createElement('a-box')
-		figure1.setAttribute('position', {x:0,y:0.1,z:-0.15});
-		figure1.setAttribute('width', '0.1');
-		figure1.setAttribute('height', '0.1');
-		figure1.setAttribute('depth', '0.1');
+		figure1.setAttribute('position', {x:0,y:0.3,z:-0.3});
+		figure1.setAttribute('width', '0.2');
+		figure1.setAttribute('height', '0.2');
+		figure1.setAttribute('depth', '0.2');
 		figure1.setAttribute('color', 'blue');
 		figure1.setAttribute('id', 'cubeClick');
 		figure1.setAttribute('class', 'button')
@@ -201,8 +214,8 @@ AFRAME.registerComponent('menu',{
 		console.log(figure1);
 
 		let figure2 = document.createElement('a-sphere')
-		figure2.setAttribute('position', {x:0,y:0.1,z:0.15});
-		figure2.setAttribute('radius', '0.07');
+		figure2.setAttribute('position', {x:0,y:0.3,z:0.3});
+		figure2.setAttribute('radius', '0.1');
 		figure2.setAttribute('color', 'yellow');
 		figure2.setAttribute('id', 'sphereClick');
 		figure2.setAttribute('class', 'button')
@@ -213,9 +226,9 @@ AFRAME.registerComponent('menu',{
 		console.log(figure2);
 
 		let figure3 = document.createElement('a-plane')
-		figure3.setAttribute('position', {x:-0.2,y:0.1,z:0});
-		figure3.setAttribute('width', '0.1');
-		figure3.setAttribute('height', '0.1');
+		figure3.setAttribute('position', {x:-0.3,y:0.3,z:0});
+		figure3.setAttribute('width', '0.2');
+		figure3.setAttribute('height', '0.2');
 		figure3.setAttribute('color', 'red');
 		figure3.setAttribute('id', 'planeClick');
 		figure3.setAttribute('planebutton', {active: true})
@@ -226,7 +239,7 @@ AFRAME.registerComponent('menu',{
 		console.log(figure3);
 
 		let figure4 = document.createElement('a-cylinder')
-		figure4.setAttribute('position', {x:0.2,y:0.1,z:0});
+		figure4.setAttribute('position', {x:0.3,y:0.3,z:0});
 		figure4.setAttribute('height', '0.07');
 		figure4.setAttribute('radius', '0.08');
 		figure4.setAttribute('color', 'purple');
